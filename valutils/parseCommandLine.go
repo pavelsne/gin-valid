72519f98a9bdf8e5f1ddd3cc7343a080e68a1ccf
M. Sonntag <michael.p.sonntag@gmail.com>
2018-07-09 16:00:42 +0200

    [valutils] Add IsValidPort function

--- valutils/parseCommandLine.go
+++ valutils/parseCommandLine.go
@@ -2,6 +2,22 @@ package valutils
 
 import "strconv"
 
+// IsValidPort checks whether a handed in interface
+// can be parsed into a valid server port number.
+func IsValidPort(port interface{}) bool {
+	if port == nil {
+		return false
+	}
+	checkPort, ok := port.(string)
+	if !ok || !isNumeric(checkPort) {
+		return false
+	}
+	if len(checkPort) != 4 {
+		return false
+	}
+	return true
+}
+
 func isNumeric(str string) bool {
 	_, err := strconv.Atoi(str)
 	return err == nil
