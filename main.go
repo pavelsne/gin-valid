03ca84b4866d87eb31f8df3a9a344be17a70ec6f
M. Sonntag <michael.p.sonntag@gmail.com>
2018-07-09 17:17:35 +0200

    [main] Move validate func to web

--- main.go
+++ main.go
@@ -1,18 +1,16 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
-	"io/ioutil"
 	"net/http"
 	"os"
-	"os/exec"
 
 	"github.com/docopt/docopt-go"
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/mpsonntag/gin-valid/config"
 	"github.com/mpsonntag/gin-valid/valutils"
+	"github.com/mpsonntag/gin-valid/web"
 )
 
 const usage = `Server validating BIDS files
@@ -32,54 +30,9 @@ func root(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "ginvalid running")
 }
 
-func validate(w http.ResponseWriter, r *http.Request) {
-	user := mux.Vars(r)["user"]
-	repo := mux.Vars(r)["repo"]
-	fmt.Fprintf(w, "validate repo '%s/%s'\n", user, repo)
-
-	cmd := exec.Command("gin", "repoinfo", fmt.Sprintf("%s/%s", user, repo))
-	if err := cmd.Run(); err != nil {
-		fmt.Fprintf(os.Stderr, "[Error] accessing '%s/%s': '%s'\n", user, repo, err.Error())
-		return
-	}
-
-	tmpdir, err := ioutil.TempDir("/home/msonntag/Chaos/DL/val", "bidsval_")
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "[Error] creating temporary directory: '%s'\n", err.Error())
-		return
-	}
-	fmt.Fprintf(w, "Directory created: %s\n", tmpdir)
-
-	// enable cleanup once tried and tested
-	defer os.RemoveAll(tmpdir)
-
-	cmd = exec.Command("gin", "get", fmt.Sprintf("%s/%s", user, repo))
-	var out bytes.Buffer
-	cmd.Stdout = &out
-	cmd.Dir = tmpdir
-	if err = cmd.Run(); err != nil {
-		fmt.Fprintf(os.Stderr, "[Error] running gin get: '%s'\n", err.Error())
-		return
-	}
-	fmt.Fprintf(w, "running in %s, gin get: %s\n", cmd.Dir, out.String())
-
-	// Ignoring NiftiHeaders for now, since it seems to be a common error
-	cmd = exec.Command("/home/msonntag/node_modules/.bin/bids-validator", "--ignoreNiftiHeaders", "--json", fmt.Sprintf("%s/%s", tmpdir, repo))
-	out.Reset()
-	cmd.Stdout = &out
-	var serr bytes.Buffer
-	cmd.Stderr = &serr
-	cmd.Dir = tmpdir
-	if err = cmd.Run(); err != nil {
-		fmt.Fprintf(os.Stderr, "[Error] running bids validation (%s): '%s', '%s', '%s'", fmt.Sprintf("%s/%s", tmpdir, repo), err.Error(), serr.String(), out.String())
-		return
-	}
-	fmt.Fprintf(w, "validation successful: \n%s\n", out.String())
-}
-
 func registerRoutes(r *mux.Router) {
 	r.HandleFunc("/", root)
-	r.HandleFunc("/validate/{user}/{repo}", validate)
+	r.HandleFunc("/validate/{user}/{repo}", web.Validate)
 }
 
 func main() {
