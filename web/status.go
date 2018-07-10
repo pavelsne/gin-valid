d85afd757435c93d39e605dc75ebff5ab5903992
M. Sonntag <michael.p.sonntag@gmail.com>
2018-07-10 17:00:12 +0200

    [web/status] Simplify existing file checks

--- web/status.go
+++ web/status.go
@@ -12,7 +12,6 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/mpsonntag/gin-valid/config"
 	"github.com/mpsonntag/gin-valid/resources"
-	"github.com/mpsonntag/gin-valid/valutils"
 )
 
 // Status returns the status of the latest BIDS validation for
@@ -23,17 +22,10 @@ func Status(w http.ResponseWriter, r *http.Request) {
 	user := mux.Vars(r)["user"]
 	repo := mux.Vars(r)["repo"]
 
-	// Check whether this repo has ever been built
-	latestPath := filepath.Join(srvconfig.Dir.Result, user, repo, "latest")
-	if !valutils.ValidDirectory(latestPath) {
-		http.ServeContent(w, r, "unavailable.svg", time.Now(), bytes.NewReader([]byte(resources.BidsUnavailable)))
-		return
-	}
-
 	fp := filepath.Join(srvconfig.Dir.Result, user, repo, "latest", "results.svg")
 	content, err := ioutil.ReadFile(fp)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "[Error] unable to serve '%s/%s' badge: %s\n", user, repo, err.Error())
+		fmt.Fprintf(os.Stderr, "[Error] serving '%s/%s' status: %s\n", user, repo, err.Error())
 		http.ServeContent(w, r, "unavailable.svg", time.Now(), bytes.NewReader([]byte(resources.BidsUnavailable)))
 		return
 	}
