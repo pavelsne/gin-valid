49178499356baa54524cecf2942fe5c36435ff4d
M. Sonntag <michael.p.sonntag@gmail.com>
2018-07-10 18:55:04 +0200

    [log] Add trim function

--- log/log.go
+++ log/log.go
@@ -11,6 +11,26 @@ import (
 var logfile *os.File
 var logger *log.Logger
 
+// trim reduces the size of a file to the provided filesize.
+// It reads the contents and writes them back, removing the
+// initial bytes to fit the limit. If any error occurs, it returns silently.
+func trim(file *os.File, filesize int) {
+	filestat, err := file.Stat()
+	if err != nil {
+		return
+	}
+	if filestat.Size() < int64(filesize) {
+		return
+	}
+	contents := make([]byte, filestat.Size())
+	nbytes, err := file.ReadAt(contents, 0)
+	if err != nil {
+		return
+	}
+	file.Truncate(0)
+	file.Write(contents[nbytes-filesize : nbytes])
+}
+
 // Init initialises log file and logger.
 func Init() error {
 	srvcfg := config.Read()
