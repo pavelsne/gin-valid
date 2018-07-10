e896838f8fc8ce52067db465af7389b0cad32d28
M. Sonntag <michael.p.sonntag@gmail.com>
2018-07-10 16:53:45 +0200

    [web/status] Serve repo status

--- web/status.go
+++ web/status.go
@@ -2,7 +2,10 @@ package web
 
 import (
 	"bytes"
+	"fmt"
+	"io/ioutil"
 	"net/http"
+	"os"
 	"path/filepath"
 	"time"
 
@@ -26,4 +29,13 @@ func Status(w http.ResponseWriter, r *http.Request) {
 		http.ServeContent(w, r, "unavailable.svg", time.Now(), bytes.NewReader([]byte(resources.BidsUnavailable)))
 		return
 	}
+
+	fp := filepath.Join(srvconfig.Dir.Result, user, repo, "latest", "results.svg")
+	content, err := ioutil.ReadFile(fp)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "[Error] unable to serve '%s/%s' badge: %s\n", user, repo, err.Error())
+		http.ServeContent(w, r, "unavailable.svg", time.Now(), bytes.NewReader([]byte(resources.BidsUnavailable)))
+		return
+	}
+	http.ServeContent(w, r, "results.svg", time.Now(), bytes.NewReader(content))
 }
