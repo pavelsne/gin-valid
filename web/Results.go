a986d18d16168670ebb1133953accd8412ebc091
M. Sonntag <michael.p.sonntag@gmail.com>
2018-07-11 17:49:54 +0200

    [web] Read results file

--- web/Results.go
+++ web/Results.go
@@ -1,9 +1,14 @@
 package web
 
 import (
+	"bytes"
+	"io/ioutil"
 	"net/http"
+	"path/filepath"
+	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/mpsonntag/gin-valid/config"
 	"github.com/mpsonntag/gin-valid/log"
 )
 
@@ -12,5 +17,14 @@ func Results(w http.ResponseWriter, r *http.Request) {
 	user := mux.Vars(r)["user"]
 	repo := mux.Vars(r)["repo"]
 
-	log.ShowWrite("[Info] delivering validation results for '%s/%s'", user, repo)
+	srvcfg := config.Read()
+	fp := filepath.Join(srvcfg.Dir.Result, user, repo, srvcfg.Label.ResultsFolder, srvcfg.Label.ResultsFile)
+	content, err := ioutil.ReadFile(fp)
+	if err != nil {
+		log.Write("[Error] serving '%s/%s' result: %s\n", user, repo, err.Error())
+		http.ServeContent(w, r, "unavailable", time.Now(), bytes.NewReader([]byte("404 Nothing to see here...")))
+		return
+	}
+
+	http.ServeContent(w, r, "results", time.Now(), bytes.NewReader(content))
 }
