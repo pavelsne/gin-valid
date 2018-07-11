bcb5c093a1a48f9621222ddd680c499d2c624dc6
M. Sonntag <michael.p.sonntag@gmail.com>
2018-07-11 17:58:00 +0200

    [web] Add status log entry

--- web/status.go
+++ web/status.go
@@ -16,17 +16,17 @@ import (
 // Status returns the status of the latest BIDS validation for
 // a provided gin user repository.
 func Status(w http.ResponseWriter, r *http.Request) {
-	srvconfig := config.Read()
-
 	user := mux.Vars(r)["user"]
 	repo := mux.Vars(r)["repo"]
+	log.Write("[Info] status for repo '%s/%s'\n", user, repo)
 
-	fp := filepath.Join(srvconfig.Dir.Result, user, repo, srvconfig.Label.ResultsFolder, srvconfig.Label.ResultsBadge)
+	srvcfg := config.Read()
+	fp := filepath.Join(srvcfg.Dir.Result, user, repo, srvcfg.Label.ResultsFolder, srvcfg.Label.ResultsBadge)
 	content, err := ioutil.ReadFile(fp)
 	if err != nil {
 		log.Write("[Error] serving '%s/%s' status: %s\n", user, repo, err.Error())
 		http.ServeContent(w, r, "unavailable.svg", time.Now(), bytes.NewReader([]byte(resources.BidsUnavailable)))
 		return
 	}
-	http.ServeContent(w, r, srvconfig.Label.ResultsBadge, time.Now(), bytes.NewReader(content))
+	http.ServeContent(w, r, srvcfg.Label.ResultsBadge, time.Now(), bytes.NewReader(content))
 }
