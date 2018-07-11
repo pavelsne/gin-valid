817aa68cea0a66af832465e78bb6d5b74b8f2280
M. Sonntag <michael.p.sonntag@gmail.com>
2018-07-11 18:50:14 +0200

    [web] Add full BIDS warnings results struct

--- web/Results.go
+++ web/Results.go
@@ -13,6 +13,63 @@ import (
 	"github.com/mpsonntag/gin-valid/log"
 )
 
+// BidsResultStruct is the struct to parse a full BIDS validation json.
+type BidsResultStruct struct {
+	Issues struct {
+		Errors   []interface{} `json:"errors"`
+		Warnings []struct {
+			Key      string `json:"key"`
+			Severity string `json:"severity"`
+			Reason   string `json:"reason"`
+			Files    []struct {
+				Key  string `json:"key"`
+				Code int    `json:"code"`
+				File struct {
+					Name         string `json:"name"`
+					Path         string `json:"path"`
+					RelativePath string `json:"relativePath"`
+					Stats        struct {
+						Dev         int       `json:"dev"`
+						Mode        int       `json:"mode"`
+						Nlink       int       `json:"nlink"`
+						UID         int       `json:"uid"`
+						Gid         int       `json:"gid"`
+						Rdev        int       `json:"rdev"`
+						Blksize     int       `json:"blksize"`
+						Ino         int       `json:"ino"`
+						Size        int       `json:"size"`
+						Blocks      int       `json:"blocks"`
+						AtimeMs     float64   `json:"atimeMs"`
+						MtimeMs     float64   `json:"mtimeMs"`
+						CtimeMs     float64   `json:"ctimeMs"`
+						BirthtimeMs float64   `json:"birthtimeMs"`
+						Atime       time.Time `json:"atime"`
+						Mtime       time.Time `json:"mtime"`
+						Ctime       time.Time `json:"ctime"`
+						Birthtime   time.Time `json:"birthtime"`
+					} `json:"stats"`
+				} `json:"file"`
+				Evidence  interface{} `json:"evidence"`
+				Line      interface{} `json:"line"`
+				Character interface{} `json:"character"`
+				Severity  string      `json:"severity"`
+				Reason    string      `json:"reason"`
+			} `json:"files"`
+			AdditionalFileCount int    `json:"additionalFileCount"`
+			Code                string `json:"code"`
+		} `json:"warnings"`
+		Ignored []interface{} `json:"ignored"`
+	} `json:"issues"`
+	Summary struct {
+		Sessions   []interface{} `json:"sessions"`
+		Subjects   []string      `json:"subjects"`
+		Tasks      []string      `json:"tasks"`
+		Modalities []string      `json:"modalities"`
+		TotalFiles int           `json:"totalFiles"`
+		Size       int           `json:"size"`
+	} `json:"summary"`
+}
+
 // Results returns the results of a previously run BIDS validation.
 func Results(w http.ResponseWriter, r *http.Request) {
 	user := mux.Vars(r)["user"]
