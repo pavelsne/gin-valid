e49b3eafa5f9b8c3005ad30440a6c179cc96fd6f
M. Sonntag <michael.p.sonntag@gmail.com>
2018-07-18 19:52:54 +0200

    [status] Update results path

--- web/status.go
+++ web/status.go
@@ -16,12 +16,13 @@ import (
 // Status returns the status of the latest BIDS validation for
 // a provided gin user repository.
 func Status(w http.ResponseWriter, r *http.Request) {
+	srvcfg := config.Read()
+
 	user := mux.Vars(r)["user"]
 	repo := mux.Vars(r)["repo"]
 	log.Write("[Info] status for repo '%s/%s'\n", user, repo)
 
-	srvcfg := config.Read()
-	fp := filepath.Join(srvcfg.Dir.Result, user, repo, srvcfg.Label.ResultsFolder, srvcfg.Label.ResultsBadge)
+	fp := filepath.Join(srvcfg.Dir.Result, "bids", user, repo, srvcfg.Label.ResultsFolder, srvcfg.Label.ResultsBadge)
 	content, err := ioutil.ReadFile(fp)
 	if err != nil {
 		log.Write("[Error] serving '%s/%s' status: %s\n", user, repo, err.Error())
