404794d4f502cf14da6adf65247b773bf67fcf44
Achilleas Koutsou <achilleas.k@gmail.com>
2018-08-06 12:16:33 +0200

    [makehook] Use gin-valid logger

--- web/makehook.go
+++ web/makehook.go
@@ -2,26 +2,24 @@ package web
 
 import (
 	"fmt"
-	"io/ioutil"
-	"log"
-	"os"
 
 	"github.com/G-Node/gin-cli/ginclient"
+	"github.com/G-Node/gin-valid/log"
 	gogs "github.com/gogits/go-gogs-client"
 )
 
-const serveralias = "gin" // change to "gin" for live server
+const serveralias = "localgogs" // change to "gin" for live server
 
+// TODO: Return error
 func createValidHook(repopath string) {
-	fmt.Printf("Adding hook to %s\n", repopath)
+	log.Write("Adding hook to %s\n", repopath)
 
 	client := ginclient.New(serveralias)
 	err := client.LoadToken()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "[error] failed to load token %s\n", err.Error())
-		log.Fatal()
+		log.Write("[error] failed to load token %s\n", err.Error())
+		return
 	}
-	fmt.Printf("Loaded token %s\n", client.Token)
 	config := make(map[string]string)
 	config["url"] = "https://example.com"
 	config["content_type"] = "json"
@@ -32,30 +30,31 @@ func createValidHook(repopath string) {
 	}
 	res, err := client.Post(fmt.Sprintf("/api/v1/repos/%s/hooks", repopath), data)
 	if err != nil {
-		log.Fatal(fmt.Sprintf("[error] bad response from server %s\n", err.Error()))
+		log.Write("[error] bad response from server %s\n", err.Error())
+		return
 	}
 	defer res.Body.Close()
-	fmt.Printf("Got response: %s\n", res.Status)
-	bdy, _ := ioutil.ReadAll(res.Body)
-	fmt.Println(string(bdy))
+	// log.Write("Got response: %s\n", res.Status)
+	// bdy, _ := ioutil.ReadAll(res.Body)
+	// fmt.Println(string(bdy))
 }
 
 func deleteValidHook(repopath string, id int) {
-	fmt.Printf("Deleting %d from %s\n", id, repopath)
+	log.Write("Deleting %d from %s\n", id, repopath)
 
 	client := ginclient.New(serveralias)
 	err := client.LoadToken()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "[error] failed to load token %s\n", err.Error())
-		log.Fatal()
+		log.Write("[error] failed to load token %s\n", err.Error())
+		return
 	}
-	fmt.Printf("Loaded token %s\n", client.Token)
 	res, err := client.Delete(fmt.Sprintf("/api/v1/repos/%s/hooks/%d", repopath, id))
 	if err != nil {
-		log.Fatal(fmt.Sprintf("[error] bad response from server %s\n", err.Error()))
+		log.Write("[error] bad response from server %s\n", err.Error())
+		return
 	}
 	defer res.Body.Close()
-	fmt.Printf("Got response: %s\n", res.Status)
-	bdy, _ := ioutil.ReadAll(res.Body)
-	fmt.Println(string(bdy))
+	// fmt.Printf("Got response: %s\n", res.Status)
+	// bdy, _ := ioutil.ReadAll(res.Body)
+	// fmt.Println(string(bdy))
 }
