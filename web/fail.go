6c1153e5189489688dd2099867d4e053cf904403
Achilleas Koutsou <achilleas.k@gmail.com>
2018-08-21 15:09:15 +0200

    Render error pages in template
    Error pages are now rendered within the standard page layout of the
    site. The status code and standard status text (e.g., Not Found) is
    displayed at the top with the more descriptive error message printed in
    red bold text.
    
    Closes #6

--- web/fail.go
+++ web/fail.go
@@ -1,9 +1,11 @@
 package web
 
 import (
+	"html/template"
 	"net/http"
 
 	"github.com/G-Node/gin-valid/log"
+	"github.com/G-Node/gin-valid/resources/templates"
 )
 
 // fail logs an error and renders an error page with the given message,
@@ -11,5 +13,27 @@ import (
 func fail(w http.ResponseWriter, status int, message string) {
 	log.Write("[error] %s", message)
 	w.WriteHeader(status)
-	w.Write([]byte(message))
+
+	tmpl := template.New("layout")
+	tmpl, err := tmpl.Parse(templates.Layout)
+	if err != nil {
+		log.Write("[Error] failed to parse html layout page. Displaying error message without layout.")
+		tmpl = template.New("content")
+	}
+	tmpl, err = tmpl.Parse(templates.Fail)
+	if err != nil {
+		log.Write("[Error] failed to render fail page. Displaying plain error message.")
+		w.Write([]byte(message))
+		return
+	}
+	errinfo := struct {
+		StatusCode int
+		StatusText string
+		Message    string
+	}{
+		status,
+		http.StatusText(status),
+		message,
+	}
+	tmpl.Execute(w, &errinfo)
 }
