10b390d8c22d2aa3263a478b746b20d19a00edfa
Achilleas Koutsou <achilleas.k@gmail.com>
2019-08-19 17:42:37 +0200

    Layout and Login: Copy from GIN
    Copied (and stripped) GIN templates for layout (header & footer) and
    login page.  Removed javascript includes and cleaned out unnecessary
    meta tags.

--- internal/resources/templates/login.go
+++ internal/resources/templates/login.go
@@ -3,17 +3,31 @@ package templates
 // TODO: add csrf thingie
 var Login = `
 {{ define "content" }}
-
-Please log in using your GIN credentials
-<hr>
-<div>
-    <form action="/login" method="post">
-        <p>Username: <input type="text" name="username"></p>
-        <p>Password: <input type="password" name="password"></p>
-        <input type="submit" value="Submit">
-    </form>
-</div>
-
-
+			<div class="user signin">
+				<div class="ui middle very relaxed page grid">
+					<div class="column">
+						<form class="ui form" action="/login" method="post">
+							<input type="hidden" name="_csrf" value="">
+							<h3 class="ui top attached header">
+								Sign In using your GIN credentials
+							</h3>
+							<div class="ui attached segment">
+								<div class="required inline field ">
+									<label for="username">Username or email</label>
+									<input id="username" name="username" value="" autofocus required>
+								</div>
+								<div class="required inline field ">
+									<label for="password">Password</label>
+									<input id="password" name="password" type="password" autocomplete="off" value="" required>
+								</div>
+								<div class="inline field">
+									<label></label>
+									<button class="ui green button">Sign In</button>
+								</div>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
 {{ end }}
 `
