3e2e1ac427559cc63abe1ddf76993b6cb8eb5b3d
Martin Cervenka <cervemar@kiv.zcu.cz>
2022-06-15 08:57:50 +0200

    [validate.go] Testing hook secret

--- internal/web/validate_test.go
+++ internal/web/validate_test.go
@@ -4,15 +4,28 @@ import (
 	//"fmt"
 	//"io"
 	//"log"
+	"github.com/G-Node/gin-valid/internal/config"
 	"net/http"
 	"net/http/httptest"
 	"strings"
 	"testing"
 )
 
-func TestFirst(t *testing.T) {
+func TestHookSecretOK(t *testing.T) {
 	r, _ := http.NewRequest("GET", "wtf", strings.NewReader("{}"))
+	srvcfg := config.Read()
+	srvcfg.Settings.HookSecret = "hooksecret"
+	config.Set(srvcfg)
 	r.Header.Add("X-Gogs-Signature", "hooksecret")
+	w := httptest.NewRecorder()
+	Validate(w, r)
+}
+func TestHokkSecretFailed(t *testing.T) {
+	r, _ := http.NewRequest("GET", "wtf", strings.NewReader("{}"))
+	srvcfg := config.Read()
+	srvcfg.Settings.HookSecret = "hooksecret"
+	config.Set(srvcfg)
+	r.Header.Add("X-Gogs-Signature", "wtf")
 	w := httptest.NewRecorder()
 	Validate(w, r)
 }
