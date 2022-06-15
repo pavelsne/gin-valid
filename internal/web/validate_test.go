09bc952ed3e820d94387d82626c3df2535c465d6
Martin Cervenka <cervemar@kiv.zcu.cz>
2022-06-15 10:35:06 +0200

    [validation_test.go] Testing tokens

--- internal/web/validate_test.go
+++ internal/web/validate_test.go
@@ -11,12 +11,44 @@ import (
 	"github.com/G-Node/gin-valid/internal/config"
 	//"io/ioutil"
 	"errors"
+	"github.com/gorilla/mux"
 	"net/http"
 	"net/http/httptest"
 	"strings"
 	"testing"
 )
 
+func TestRepoDoesNotExists(t *testing.T) {
+	body := []byte("{}")
+	router := mux.NewRouter()
+	router.HandleFunc("/validate/{validator}/{user}/{repo}", Validate).Methods("POST")
+	login, _ := doLogin("gin-user", "student")
+	tok, _ := getTokenBySession(login)
+	saveToken(tok)
+	r, _ := http.NewRequest("POST", "/validate/bids/wtf/wtf", bytes.NewReader(body))
+	w := httptest.NewRecorder()
+	srvcfg := config.Read()
+	srvcfg.Settings.HookSecret = "hooksecret"
+	config.Set(srvcfg)
+	sig := hmac.New(sha256.New, []byte(srvcfg.Settings.HookSecret))
+	sig.Write(body)
+	r.Header.Add("X-Gogs-Signature", hex.EncodeToString(sig.Sum(nil)))
+	router.ServeHTTP(w, r)
+}
+func TestBadToken(t *testing.T) {
+	body := []byte("{}")
+	router := mux.NewRouter()
+	router.HandleFunc("/validate/{validator}/{user}/{repo}", Validate).Methods("POST")
+	r, _ := http.NewRequest("POST", "/validate/bids/cervenkam/Testing", bytes.NewReader(body))
+	w := httptest.NewRecorder()
+	srvcfg := config.Read()
+	srvcfg.Settings.HookSecret = "hooksecret"
+	config.Set(srvcfg)
+	sig := hmac.New(sha256.New, []byte(srvcfg.Settings.HookSecret))
+	sig.Write(body)
+	r.Header.Add("X-Gogs-Signature", hex.EncodeToString(sig.Sum(nil)))
+	router.ServeHTTP(w, r)
+}
 func TestUnsupportedValidator(t *testing.T) {
 	body := []byte("{}")
 	r, _ := http.NewRequest("GET", "wtf", bytes.NewReader(body))
