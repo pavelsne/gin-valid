b5a226a5a8af236c9f36fe8d8207672bf12ecac9
Martin Cervenka <cervemar@kiv.zcu.cz>
2022-06-15 13:56:58 +0200

    Revert "[results_test.go] Bad data"
    This reverts commit 329768aff2fd3605b34f026aba83afe0ef6766ef.

--- internal/web/results_test.go
+++ internal/web/results_test.go
@@ -5,37 +5,13 @@ import (
 	"crypto/hmac"
 	"crypto/sha256"
 	"encoding/hex"
-	"fmt"
 	"github.com/G-Node/gin-valid/internal/config"
 	"github.com/gorilla/mux"
 	"net/http"
 	"net/http/httptest"
-	"os"
-	"path/filepath"
 	"testing"
 )
 
-func TestResultsSomeResults(t *testing.T) {
-	username := "cervemar"
-	reponame := "Testing"
-	id := "1"
-	content := []byte("wtf")
-	body := []byte("{}")
-	router := mux.NewRouter()
-	router.HandleFunc("/results/{validator}/{user}/{repo}/{id}", Results).Methods("GET")
-	r, _ := http.NewRequest("GET", filepath.Join("/results/bids", username, "/", reponame, "/", id), bytes.NewReader(body))
-	w := httptest.NewRecorder()
-	srvcfg := config.Read()
-	fmt.Printf("%+v\n", os.Mkdir("bids", 0755))
-	os.Mkdir(filepath.Join(srvcfg.Dir.Result, "bids", username), 0755)
-	os.Mkdir(filepath.Join(srvcfg.Dir.Result, "bids", username, reponame), 0755)
-	os.Mkdir(filepath.Join(srvcfg.Dir.Result, "bids", username, reponame, id), 0755)
-	os.WriteFile(filepath.Join(srvcfg.Dir.Result, "bids", username, reponame, id, srvcfg.Label.ResultsFile), content, 0644)
-	sig := hmac.New(sha256.New, []byte(srvcfg.Settings.HookSecret))
-	sig.Write(body)
-	r.Header.Add("X-Gogs-Signature", hex.EncodeToString(sig.Sum(nil)))
-	router.ServeHTTP(w, r)
-}
 func TestResultsNoResults(t *testing.T) {
 	body := []byte("{}")
 	router := mux.NewRouter()
