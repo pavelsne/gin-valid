d0e5b25b9bdef2d17dafba01743c0003f23180c3
Martin Cervenka <cervemar@kiv.zcu.cz>
2022-06-15 08:17:02 +0200

    [validate_test.go] First working test

---
+++ internal/web/validate_test.go
@@ -0,0 +1,16 @@
+package web
+import (
+	"fmt"
+	//"io"
+	//"log"
+	"net/http/httptest"
+	"net/http"
+	"testing"
+	"strings"
+)
+func TestFirst(t *testing.T) {
+	r,_ := http.NewRequest("GET","wtf",strings.NewReader("{}"))
+	r.Header.Add("X-Gogs-Signature","hooksecret")
+	w := httptest.NewRecorder()
+	Validate(w,r)
+}
