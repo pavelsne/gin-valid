9c941efee66cde8208591e87f1a4347b9852180e
Martin Cervenka <cervemar@kiv.zcu.cz>
2022-06-20 10:03:27 +0200

    [token_test.go] [status_test.go] Finishing token and status testing

--- internal/web/status_test.go
+++ internal/web/status_test.go
@@ -5,14 +5,38 @@ import (
 	"crypto/hmac"
 	"crypto/sha256"
 	"encoding/hex"
+	"fmt"
 	"github.com/G-Node/gin-valid/internal/config"
 	"github.com/gorilla/mux"
 	"net/http"
 	"net/http/httptest"
+	"os"
+	"path/filepath"
 	"testing"
 )
 
-func TestStatus(t *testing.T) {
+func TestStatusOK(t *testing.T) {
+	content := "wtf"
+	body := []byte("{}")
+	router := mux.NewRouter()
+	router.HandleFunc("/status/{validator}/{user}/{repo}", Status).Methods("GET")
+	r, _ := http.NewRequest("GET", "/status/bids/whatever/whatever", bytes.NewReader(body))
+	w := httptest.NewRecorder()
+	srvcfg := config.Read()
+	sig := hmac.New(sha256.New, []byte(srvcfg.Settings.HookSecret))
+	sig.Write(body)
+	r.Header.Add("X-Gogs-Signature", hex.EncodeToString(sig.Sum(nil)))
+	os.Mkdir(srvcfg.Dir.Result, 0755)
+	os.Mkdir(filepath.Join(srvcfg.Dir.Result, "bids"), 0755)
+	os.Mkdir(filepath.Join(srvcfg.Dir.Result, "bids", username), 0755)
+	os.Mkdir(filepath.Join(srvcfg.Dir.Result, "bids", username, reponame), 0755)
+	os.Mkdir(filepath.Join(srvcfg.Dir.Result, "bids", username, reponame, srvcfg.Label.ResultsFolder), 0755)
+	f, e := os.Create(filepath.Join(srvcfg.Dir.Result, "bids", username, reponame, srvcfg.Label.ResultsFolder, srvcfg.Label.ResultsBadge))
+	defer f.Close()
+	f.WriteString(content)
+	router.ServeHTTP(w, r)
+}
+func TestStatusNoConent(t *testing.T) {
 	body := []byte("{}")
 	router := mux.NewRouter()
 	router.HandleFunc("/status/{validator}/{user}/{repo}", Status).Methods("GET")
