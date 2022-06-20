1fb4198097dd20fd9cba795d278e58c817e0ecf2
Martin Cervenka <cervemar@kiv.zcu.cz>
2022-06-20 13:40:06 +0200

    [results_test.go] Results testing

--- internal/web/results_test.go
+++ internal/web/results_test.go
@@ -14,6 +14,105 @@ import (
 	"testing"
 )
 
+func TestResultsUnsupportedV2(t *testing.T) {
+	id := "1"
+	content := "{\"empty\":\"json\"}"
+	body := []byte("{}")
+	router := mux.NewRouter()
+	router.HandleFunc("/results/{validator}/{user}/{repo}/{id}", Results).Methods("GET")
+	r, _ := http.NewRequest("GET", filepath.Join("/results/wtf", username, "/", reponame, "/", id), bytes.NewReader(body))
+	w := httptest.NewRecorder()
+	srvcfg := config.Read()
+	srvcfg.Settings.Validators = append(srvcfg.Settings.Validators, "wtf")
+	config.Set(srvcfg)
+	os.MkdirAll(filepath.Join(srvcfg.Dir.Result, "nix", username, reponame, id), 0755)
+	f, _ := os.Create(filepath.Join(srvcfg.Dir.Result, "nix", username, reponame, id, srvcfg.Label.ResultsFile))
+	defer f.Close()
+	f.WriteString(content)
+	sig := hmac.New(sha256.New, []byte(srvcfg.Settings.HookSecret))
+	sig.Write(body)
+	r.Header.Add("X-Gogs-Signature", hex.EncodeToString(sig.Sum(nil)))
+	router.ServeHTTP(w, r)
+	os.RemoveAll(filepath.Join(srvcfg.Dir.Result, "nix", username, reponame, id))
+	srvcfg.Settings.Validators = srvcfg.Settings.Validators[:len(srvcfg.Settings.Validators)-1]
+	config.Set(srvcfg)
+}
+func TestResultsODML(t *testing.T) {
+	id := "1"
+	content := "{\"empty\":\"json\"}"
+	body := []byte("{}")
+	router := mux.NewRouter()
+	router.HandleFunc("/results/{validator}/{user}/{repo}/{id}", Results).Methods("GET")
+	r, _ := http.NewRequest("GET", filepath.Join("/results/odml", username, "/", reponame, "/", id), bytes.NewReader(body))
+	w := httptest.NewRecorder()
+	srvcfg := config.Read()
+	os.MkdirAll(filepath.Join(srvcfg.Dir.Result, "odml", username, reponame, id), 0755)
+	f, _ := os.Create(filepath.Join(srvcfg.Dir.Result, "odml", username, reponame, id, srvcfg.Label.ResultsFile))
+	defer f.Close()
+	f.WriteString(content)
+	sig := hmac.New(sha256.New, []byte(srvcfg.Settings.HookSecret))
+	sig.Write(body)
+	r.Header.Add("X-Gogs-Signature", hex.EncodeToString(sig.Sum(nil)))
+	router.ServeHTTP(w, r)
+	os.RemoveAll(filepath.Join(srvcfg.Dir.Result, "odml", username, reponame, id))
+}
+func TestResultsNIX(t *testing.T) {
+	id := "1"
+	content := "{\"empty\":\"json\"}"
+	body := []byte("{}")
+	router := mux.NewRouter()
+	router.HandleFunc("/results/{validator}/{user}/{repo}/{id}", Results).Methods("GET")
+	r, _ := http.NewRequest("GET", filepath.Join("/results/nix", username, "/", reponame, "/", id), bytes.NewReader(body))
+	w := httptest.NewRecorder()
+	srvcfg := config.Read()
+	os.MkdirAll(filepath.Join(srvcfg.Dir.Result, "nix", username, reponame, id), 0755)
+	f, _ := os.Create(filepath.Join(srvcfg.Dir.Result, "nix", username, reponame, id, srvcfg.Label.ResultsFile))
+	defer f.Close()
+	f.WriteString(content)
+	sig := hmac.New(sha256.New, []byte(srvcfg.Settings.HookSecret))
+	sig.Write(body)
+	r.Header.Add("X-Gogs-Signature", hex.EncodeToString(sig.Sum(nil)))
+	router.ServeHTTP(w, r)
+	os.RemoveAll(filepath.Join(srvcfg.Dir.Result, "nix", username, reponame, id))
+}
+func TestResultsInJSON(t *testing.T) {
+	id := "1"
+	content := "{\"empty\":\"json\"}"
+	body := []byte("{}")
+	router := mux.NewRouter()
+	router.HandleFunc("/results/{validator}/{user}/{repo}/{id}", Results).Methods("GET")
+	r, _ := http.NewRequest("GET", filepath.Join("/results/bids", username, "/", reponame, "/", id), bytes.NewReader(body))
+	w := httptest.NewRecorder()
+	srvcfg := config.Read()
+	os.MkdirAll(filepath.Join(srvcfg.Dir.Result, "bids", username, reponame, id), 0755)
+	f, _ := os.Create(filepath.Join(srvcfg.Dir.Result, "bids", username, reponame, id, srvcfg.Label.ResultsFile))
+	defer f.Close()
+	f.WriteString(content)
+	sig := hmac.New(sha256.New, []byte(srvcfg.Settings.HookSecret))
+	sig.Write(body)
+	r.Header.Add("X-Gogs-Signature", hex.EncodeToString(sig.Sum(nil)))
+	router.ServeHTTP(w, r)
+	os.RemoveAll(filepath.Join(srvcfg.Dir.Result, "bids", username, reponame, id))
+}
+func TestResultsInProgress(t *testing.T) {
+	id := "1"
+	content := progressmsg
+	body := []byte("{}")
+	router := mux.NewRouter()
+	router.HandleFunc("/results/{validator}/{user}/{repo}/{id}", Results).Methods("GET")
+	r, _ := http.NewRequest("GET", filepath.Join("/results/bids", username, "/", reponame, "/", id), bytes.NewReader(body))
+	w := httptest.NewRecorder()
+	srvcfg := config.Read()
+	os.MkdirAll(filepath.Join(srvcfg.Dir.Result, "bids", username, reponame, id), 0755)
+	f, _ := os.Create(filepath.Join(srvcfg.Dir.Result, "bids", username, reponame, id, srvcfg.Label.ResultsFile))
+	defer f.Close()
+	f.WriteString(content)
+	sig := hmac.New(sha256.New, []byte(srvcfg.Settings.HookSecret))
+	sig.Write(body)
+	r.Header.Add("X-Gogs-Signature", hex.EncodeToString(sig.Sum(nil)))
+	router.ServeHTTP(w, r)
+	os.RemoveAll(filepath.Join(srvcfg.Dir.Result, "bids", username, reponame, id))
+}
 func TestResultsSomeResults(t *testing.T) {
 	id := "1"
 	content := "wtf"
