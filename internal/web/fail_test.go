55b431b3cd5b8d6c1ee91aff45cc16c91d236610
Martin Cervenka <cervemar@kiv.zcu.cz>
2022-06-20 12:27:36 +0200

    [user_test.go] More user-side testing

--- internal/web/fail_test.go
+++ internal/web/fail_test.go
@@ -8,11 +8,15 @@ import (
 
 func TestFailFailedToParse(t *testing.T) {
 	w := httptest.NewRecorder()
+	original := templates.Layout
 	templates.Layout = "{{ WTF? }"
 	fail(w, 200, "WTF")
+	templates.Layout = original
 }
 func TestFailFailedToParseFailPage(t *testing.T) {
 	w := httptest.NewRecorder()
+	original := templates.Fail
 	templates.Fail = "{{ WTF? }"
 	fail(w, 200, "WTF")
+	templates.Fail = original
 }
--- internal/web/user_test.go
+++ internal/web/user_test.go
@@ -1,9 +1,16 @@
 package web
 
 import (
+	"bytes"
 	"github.com/G-Node/gin-valid/internal/config"
+	"github.com/G-Node/gin-valid/internal/resources/templates"
+	"github.com/gorilla/mux"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
 	"os"
 	"path/filepath"
+	"strings"
 	"testing"
 )
 
@@ -31,4 +38,44 @@ func TestUserDoLoginOK(t *testing.T) {
 	os.MkdirAll(tokendir, 0755)
 	doLogin(username, password)
 	os.RemoveAll(tokendir)
+}
+func TestUserLoginPost(t *testing.T) {
+	data := url.Values{}
+	data.Set("username", username)
+	data.Set("password", password)
+	router := mux.NewRouter()
+	router.HandleFunc("/login", LoginPost).Methods("POST")
+	r, _ := http.NewRequest("POST", "/login", strings.NewReader(data.Encode()))
+	w := httptest.NewRecorder()
+	router.ServeHTTP(w, r)
+}
+func TestUserLoginGet(t *testing.T) {
+	body := []byte("{}")
+	router := mux.NewRouter()
+	router.HandleFunc("/login", LoginGet).Methods("GET")
+	r, _ := http.NewRequest("GET", "/login", bytes.NewReader(body))
+	w := httptest.NewRecorder()
+	router.ServeHTTP(w, r)
+}
+func TestUserLoginGetBadLoginPage(t *testing.T) {
+	original := templates.Login
+	templates.Login = "{{ WTF? }"
+	body := []byte("{}")
+	router := mux.NewRouter()
+	router.HandleFunc("/login", LoginGet).Methods("GET")
+	r, _ := http.NewRequest("GET", "/login", bytes.NewReader(body))
+	w := httptest.NewRecorder()
+	router.ServeHTTP(w, r)
+	templates.Login = original
+}
+func TestUserLoginGetBadLayout(t *testing.T) {
+	original := templates.Layout
+	templates.Layout = "{{ WTF? }"
+	body := []byte("{}")
+	router := mux.NewRouter()
+	router.HandleFunc("/login", LoginGet).Methods("GET")
+	r, _ := http.NewRequest("GET", "/login", bytes.NewReader(body))
+	w := httptest.NewRecorder()
+	router.ServeHTTP(w, r)
+	templates.Layout = original
 }
